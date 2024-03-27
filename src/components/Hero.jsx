9bbeacf58464a2a69f948a680a5b586a53189a5e
Deepak-Maan <jattdevta1008@gmail.com>
2024-03-27 11:28:34 +0530

    modified hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import heroTriangle from '../assets/images/png/hero-triangle.png'
 const Hero = () => {
     return (
         <div className='container h-header d-flex flex-column justify-content-center position-relative'>
-            <img className='hero-triangle' src={heroTriangle} alt="hero-triangle" />
+            <img className='hero-triangle d-none d-md-block' src={heroTriangle} alt="hero-triangle" />
             <h1 className='text-white fs-xxl fw-bold text-center'>Changing the way games <span className='d-block' >are made</span></h1>
             <p className='fs-lg1 fw-normal text-white  text-center'>Redefining the future of play</p>
         </div>
